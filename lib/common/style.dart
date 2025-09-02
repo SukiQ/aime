7f1e750ac1c6c8d5599fa1145f4b2bc4ca1104e5
SukiQ <66855732+SukiQ@users.noreply.github.com>
2025-09-02 18:47:19 +0800

    submit

---
+++ lib/common/style.dart
@@ -0,0 +1,7 @@
+
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFF525252);
+  static const Color background = Color(0xFFDEDEDE);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aime/common/style.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,54 +8,39 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      locale: Locale('zh', 'CN'),
+      title: 'Aime',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget{
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Demo"),
-      ),
-      body: Center(
-        child: Center(
-          child: Text(
-            "Hello World"
-          )
-          ,
+        primaryColor: AppColors.primary,
+        appBarTheme: AppBarTheme(
+          backgroundColor: AppColors.background,
+        ),
+        scaffoldBackgroundColor: AppColors.background,
+        navigationBarTheme: NavigationBarThemeData(
+          backgroundColor: AppColors.background,
+        ),
+        navigationDrawerTheme: NavigationDrawerThemeData(
+          backgroundColor: AppColors.background,
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: AppColors.background,
+          selectedItemColor: AppColors.primary,
         )
       ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Home')),
+        body: SafeArea(child: Center(child: Text("Hello World"))),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: '首页'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: '搜索'),
+          ],
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
     );
   }
-
 }
-
-
